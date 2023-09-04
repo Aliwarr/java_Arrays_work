4ef4beebd3a903cf2afa86086666b66560003f37
RAHMAT IMAM RAHBAR <91403803+Aliwarr@users.noreply.github.com>
2023-09-04 15:23:33 +0000

    first comit

---
+++ Patterns/Pattern8.java
@@ -0,0 +1,20 @@
+public class Pattern8 {
+    public static void printPattern(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<9-2*i;j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int n=5;
+        printPattern(n);
+    }
+}
